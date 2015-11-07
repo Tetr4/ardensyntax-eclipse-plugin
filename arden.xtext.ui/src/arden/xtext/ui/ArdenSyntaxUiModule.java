3f7c92943013325088171860ccdee736d426f61c
Mike Klimek <m.klimek@tu-bs.de>
2015-11-07 17:49:56 +0100

    Add better code folding
    Only categories and slot which contain statements are foldable.

--- arden.xtext.ui/src/arden/xtext/ui/ArdenSyntaxUiModule.java
+++ arden.xtext.ui/src/arden/xtext/ui/ArdenSyntaxUiModule.java
@@ -4,25 +4,32 @@
 package arden.xtext.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.folding.IFoldingRegionProvider;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
-import arden.xtext.ui.syntaxcoloring.ArdenSyntaxSemanticHighlightingCalculator;
+import arden.xtext.ui.folding.ArdenSyntaxFoldingRegionProvider;
 import arden.xtext.ui.syntaxcoloring.ArdenSyntaxHighlightingConfiguration;
+import arden.xtext.ui.syntaxcoloring.ArdenSyntaxSemanticHighlightingCalculator;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
 public class ArdenSyntaxUiModule extends arden.xtext.ui.AbstractArdenSyntaxUiModule {
-	public ArdenSyntaxUiModule(AbstractUIPlugin plugin) {
-		super(plugin);
-	}
-	
-	public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
-		return ArdenSyntaxSemanticHighlightingCalculator.class;
-	}
-	
-	public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration() {
-		return ArdenSyntaxHighlightingConfiguration.class;
-	}
+    public ArdenSyntaxUiModule(AbstractUIPlugin plugin) {
+        super(plugin);
+    }
+
+    public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
+        return ArdenSyntaxSemanticHighlightingCalculator.class;
+    }
+
+    public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration() {
+        return ArdenSyntaxHighlightingConfiguration.class;
+    }
+
+    public Class<? extends IFoldingRegionProvider> bindIFoldingRegionProvider() {
+        return ArdenSyntaxFoldingRegionProvider.class;
+    }
+
 }
